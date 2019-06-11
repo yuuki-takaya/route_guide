fd29ad9fc69574fcbfe643c0435c6049e0763e59
Yuuki Takaya <yuuki.takaya.vz@nttdocomo.com>
2019-06-11 17:12:12 +0900

    placeのresponse実装

--- routes/places.js
+++ routes/places.js
@@ -1,35 +1,189 @@
 var express = require('express');
 var router = express.Router();
-
 var places = {};
 var key = 'PlaceName';
 places[key] = [];
+const Localip = "192.168.43.78:3000";
 
-var kenkaiConferenceRoom = {
-    billboardObject: '',
-    position: '',
-    rotaiton: '',
-    scale: ''
+
+
+let kenkaiConferenceRoom = {
+    kenkai: [
+        {
+            url:'http://' + Localip + '/billboards/grandfather.png',
+            position: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            rotation: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            scale: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            }
+        },
+        {
+            url: 'http://' + Localip + '/billboards/grandfather.png',
+            position: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            rotation: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            scale: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            }
+        }
+    ],
+    toilet: [
+        {
+            url: 'http://' + Localip + '/billboards/grandfather.png',
+            position: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            rotation: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            scale: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            }
+        },
+        {
+            url: 'http://' + Localip + '/billboards/grandfather.png',
+            position: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            rotation: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            scale: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            }
+        }
+    ],
+    elevator: [
+        {
+            url:'http://' + Localip + '/billboards/grandfather.png',
+            position: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            rotation: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            scale: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            }
+        },
+        {
+            url: 'http://' + Localip + '/billboards/grandfather.png',
+            position: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            rotation: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            scale: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            }
+        }
+    ],
+    reflesh: [
+        {
+            url: 'http://' + Localip + '/billboards/grandfather.png',
+            position: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            rotation: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            scale: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            }
+        },
+        {
+            url: 'http://' + Localip + '/billboards/grandfather.png',
+            position: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            rotation: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            },
+            scale: {
+                'x': 0.1,
+                'y': 0.2, 
+                'z': 0.3
+            }
+        }
+    ]
 }
-
 places[key].push(kenkaiConferenceRoom);
-
-// [
-//     {
-//         billboard.obj
-//         Pos
-//         Rot
-//         Scal
-//         url:~
-//     },
-// ]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
   res.setHeader('Content-Type', 'application/json');
-  res.json({ "places": places});
-  console.log(req.query);
+  console.log(req.query.place);
+  let placeName = req.query.place;
+  if(req.query.place == 'kenkai'){
+    console.log(places.PlaceName[0].kenkai);
+    res.send(places.PlaceName[0].kenkai);
+  }else if(req.query.place == 'toilet'){
+    console.log(places.PlaceName[0].toilet);
+    res.send(places.PlaceName[0].toilet);
+  }else if(req.query.place == 'elevator'){
+    console.log(places.PlaceName[0].elevator);
+    res.send(places.PlaceName[0].elevator);
+  }else if(req.query.place == 'reflesh'){
+    console.log(places.PlaceName[0].reflesh);
+    res.send(places.PlaceName[0].reflesh);
+  }else{
+    res.send("Your Request Place is not Registered");
+  }
+    
 });
-
 module.exports = router;
