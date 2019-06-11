77f452449615edeb588a6575646d0a10179beeff
Yuuki Takaya <yuuki.takaya.vz@nttdocomo.com>
2019-06-11 17:59:05 +0900

    フォーム追加（UIのみ）

--- routes/index.js
+++ routes/index.js
@@ -19,12 +19,7 @@ const uploadDir = multer({storage: storage});
 
 // HTMLファイル側はフォームでアップロードファイルを選択させます
 router.get('/', (req, res) => { 
-  res.send(
-    '<form method="POST" action="/" enctype="multipart/form-data">' +
-      '<input type="file" name="upFile" /><br />' +
-      '<input type="submit" value="アップロード" />' +
-    '</form>'
-  );
+  res.render('index', { title: 'Express' });
 });
 
 // Express 受信側
@@ -32,6 +27,8 @@ router.post('/', uploadDir.single('upFile'), (req, res) => {
   console.log('アップロードしたファイル名： ' + req.file.originalname);
   console.log('保存されたパス：' + req.file.path);
   console.log('保存されたファイル名： ' + req.file.filename);
+  console.log(req.body.upText);
+  console.log(req.body.save_directory);
 });
 
 module.exports = router;
