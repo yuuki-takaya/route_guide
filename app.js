f7e84a2b482b22aa1c1aa082ee709aebda612e86
Yuuki Takaya <yuuki.takaya.vz@nttdocomo.com>
2019-06-11 13:12:20 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const http = require('http');
+// const mongoose = require('mongoose');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var placesRouter = require('./routes/places');
+var characterRouter = require('./routes/characters')
+
+var app = express();
+
+// Database setup
+// mongoose.Promise = global.Promise;
+// const mongodbUri = 'mongodb://localhost/dekiru';
+// const mongOptions = {
+//     useMongoClient: true,
+//     socketTimeoutMS: 0,
+//     keepAlive: true,
+//     reconnectTries: 30
+// };
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// ポート設定
+app.set('httpport', process.env.PORT || 3000);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/places', placesRouter);
+app.use('/characters',characterRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var server = http.createServer(app).listen(app.get('httpport'), function(){
+  console.log('Express HTTP server listening on port ' + app.get('httpport'));
+  // mongoose.connect(mongodbUri, mongOptions);
+});
+
+module.exports = app;
---
+++ routes/places.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+var places = {};
+var key = 'PlaceName';
+places[key] = [];
+
+var kenkaiConferenceRoom = {
+    billboardObject: '',
+    position: '',
+    rotaiton: '',
+    scale: ''
+}
+
+places[key].push(kenkaiConferenceRoom);
+
+// [
+//     {
+//         billboard.obj
+//         Pos
+//         Rot
+//         Scal
+//         url:~
+//     },
+// ]
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ "places": places});
+  console.log(req.query);
+});
+
+module.exports = router;
